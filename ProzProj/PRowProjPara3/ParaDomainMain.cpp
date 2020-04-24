1f4b34c6309cf51571657f878303c33eedc52c4f
BartoszCie <Miketen1997@gmail.com>
2020-04-24 17:56:03 +0200

    heeeeeeeelelelle

--- ProzProj/PRowProjPara3/ParaDomainMain.cpp
+++ ProzProj/PRowProjPara3/ParaDomainMain.cpp
@@ -167,12 +167,92 @@ int generatePrimesSeq(int table[], int size, int max, int min, int result[], int
 	return count;
 }
 
+int generatePrimesSeqWithoutUsingPrimes(int table[], int size, int max, int min, int result[], int numOfThreads)
+{
+	omp_set_num_threads(numOfThreads);
+	int sqrtMax = (int)sqrt(max);
+	//wykreslanie
+#pragma omp parallel
+	{
+#pragma omp for
+		for (int i = 2; i < max / 2; i++)
+		{
+			for (int p = 2; p <= sqrtMax; p++)
+			{
+				if (p * i - 2 <= max)
+				{
+					if (table[p * i - 2] != -1)
+					{
+						table[p * i - 2] = -1;
+					}
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+	}
+
+	int count = 0;
+
+	for (int i = 0; i <= max - 2; i++)
+	{
+		int currentNumber = table[i];
+		if (currentNumber != -1 && currentNumber >= min)
+		{
+			result[count++] = currentNumber;
+		}
+	}
+
+	return count;
+}
+
+
+int generatePrimesSeqActualDomain(int table[], int size, int max, int result[]){
+	int primesCount = 0;
+	int *temp = new int[max];
+
+	if (max <= 2)
+	{
+		result[0] = 2;
+		return 1;
+	}
+	primesCount = generatePrimesSeqActualDomain(table, size, (int)sqrt(max), temp);
+
+	//wykreslanie
+	for (int p = 0; p < primesCount; p++)
+	{
+		int i = 2;
+		int prime = temp[p];
+		while (prime * i <= max)
+		{
+			table[prime * i - 2] = -1;
+			i++;
+		}
+	}
+
+	//wpisywanie liczb pierwszych do tablicy przekazywanej w gore
+	int count = 0;
+
+	for (int i = 0; i <= max - 2; i++)
+	{
+		int currentNumber = table[i];
+		if (currentNumber != -1)
+		{
+			result[count++] = currentNumber;
+		}
+	}
+
+	delete[] temp;
+	return count;
+}
 
 int generatePrimesSeqActualDomain(int table[], int size, int max, int min, int result[], int numOfThreads)
 {
 	int *temp = new int[max - min + 1];
 	int primesCount = 0;
-	primesCount = generatePrimesSeq(table, size, (int)sqrt(max), temp);
+	primesCount = generatePrimesSeqActualDomain(table, size, (int)sqrt(max), temp);
 	omp_set_num_threads(numOfThreads);
 #pragma omp parallel
 	{
@@ -221,30 +301,39 @@ int generatePrimesSeqActualDomain(int table[], int size, int max, int min, int r
 	return count;
 }
 
-
-int generatePrimesSeqWithoutUsingPrimes(int table[], int size, int max, int min, int result[], int numOfThreads)
+int generatePrimesSeqActualDomainWithoutUsingPrimes(int table[], int size, int max, int min, int result[], int numOfThreads)
 {
 	omp_set_num_threads(numOfThreads);
 	int sqrtMax = (int)sqrt(max);
 	//wykreslanie
 #pragma omp parallel
 	{
-#pragma omp for
-		for (int i = 2; i < max / 2; i++)
+		int myThreadNum = omp_get_thread_num();
+		int myStartingPoint = 0;
+		int myEndPoint = 0;
+		if(size % numOfThreads == 0){
+			myStartingPoint = (size/numOfThreads) * myThreadNum;
+			myEndPoint = myStartingPoint + (size/numOfThreads) - 1;
+		}else{
+			myStartingPoint = (size/numOfThreads + 1) * myThreadNum;
+			myEndPoint = myStartingPoint + (size/numOfThreads + 1) - 1;
+		}
+		
+		for (int p = 2; p < sqrtMax; p++)
 		{
-			for (int p = 2; p <= sqrtMax; p++)
+			int i = 0;
+			if(myStartingPoint % p == 0){
+				i = myStartingPoint/p;
+			}else{
+				i = myStartingPoint/p + 1;
+			}
+			if(i == 0){
+				i+=2;
+			}
+			while (p * i <= myEndPoint + 2)
 			{
-				if (p * i - 2 <= max)
-				{
-					if (table[p * i - 2] != -1)
-					{
-						table[p * i - 2] = -1;
-					}
-				}
-				else
-				{
-					break;
-				}
+				table[p * i - 2] = -1;
+				i++;
 			}
 		}
 	}
@@ -299,15 +388,46 @@ void testPrimesSeqWithoutUsingPrimes(int maxNumber, int minNumber, int numOfThre
 	int *resultNoPrimes = new int[maxNumber - minNumber + 1]();
 	int *numbers = generateSequence(2, maxNumber);
 	start = clock();
-	int primesCount = generatePrimesSeqActualDomain(numbers, maxNumber - 2 + 1, maxNumber, minNumber, resultNoPrimes, numOfThreads);
+	int primesCount = generatePrimesSeqWithoutUsingPrimes(numbers, maxNumber - 2 + 1, maxNumber, minNumber, resultNoPrimes, numOfThreads);
 	stop = clock();
 	printf("%d\n", primesCount);
 	printf("Czas przetwarzania dla wersji bez generowania liczb pierwszych wynosi %f sekund\n", ((double)(stop - start) / 1000.0));
 
-	//delete[] numbers;
-	//delete[] resultNoPrimes;
+	delete[] numbers;
+	delete[] resultNoPrimes;
 }
 
+
+
+void testPrimesSeqWithActualDomain(int maxNumber, int minNumber, int numOfThreads)
+{
+	clock_t start, stop;
+	int *resultNoPrimes = new int[maxNumber - minNumber + 1]();
+	int *numbers = generateSequence(2, maxNumber);
+	start = clock();
+	int primesCount = generatePrimesSeqActualDomain(numbers, maxNumber - 2 + 1, maxNumber, minNumber, resultNoPrimes, numOfThreads);
+	stop = clock();
+	printf("%d\n", primesCount);
+	printf("Czas przetwarzania dla wersji domenowej liczb pierwszych wynosi %f sekund\n", ((double)(stop - start) / 1000.0));
+
+	delete[] numbers;
+	delete[] resultNoPrimes;
+}
+
+void testPrimesSeqWithActualDomainNoPrimes(int maxNumber, int minNumber, int numOfThreads)
+{
+	clock_t start, stop;
+	int *resultNoPrimes = new int[maxNumber - minNumber + 1]();
+	int *numbers = generateSequence(2, maxNumber);
+	start = clock();
+	int primesCount = generatePrimesSeqActualDomainWithoutUsingPrimes(numbers, maxNumber - 2 + 1, maxNumber, minNumber, resultNoPrimes, numOfThreads);
+	stop = clock();
+	printf("%d\n", primesCount);
+	printf("Czas przetwarzania dla wersji domenowej liczb pierwszych bez u¿ywania liczb pierwszych wynosi %f sekund\n", ((double)(stop - start) / 1000.0));
+
+	delete[] numbers;
+	delete[] resultNoPrimes;
+}
 
 int main()
 {
@@ -318,5 +438,7 @@ int main()
 	testMultiParallelGeneration(maxNumber, minNumber, numOfThreads);
 	testAfterRecursionParallellGeneration(maxNumber, minNumber, numOfThreads);
 	testPrimesSeqWithoutUsingPrimes(maxNumber, minNumber, numOfThreads);
+	testPrimesSeqWithActualDomain(maxNumber, minNumber, numOfThreads);
+	testPrimesSeqWithActualDomainNoPrimes(maxNumber, minNumber, numOfThreads);
 	return 0;
 }
